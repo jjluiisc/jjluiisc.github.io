371ff81f56135d85b3a3e2baf103d493431dc1d7
jjluiisc <75454478+jjluiisc@users.noreply.github.com>
2023-02-02 22:36:25 -0600

    Update sistema.js

--- js/sistema.js
+++ js/sistema.js
@@ -15,3 +15,9 @@ function MostrarSoloPersonaje(){
 function MostrarTodo(){
 	$('.otrosPersonajes').show();
 }
+
+function Pdf(id,fileName){
+	var doc = new jsPDF();
+	doc.fromHTML(document.getElementById(id).innerHTML.replaceAll("<salto>","</br>"),5,5);
+	doc.save(fileName+".pdf");
+}
