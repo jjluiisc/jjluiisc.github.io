b9ecff2fd3a3fcb19fb8dbf6727d03626ad2ba08
jjluiisc <75454478+jjluiisc@users.noreply.github.com>
2023-10-23 22:33:34 -0600

    Update libreto.js

--- js/libreto.js
+++ js/libreto.js
@@ -12,11 +12,11 @@ var primer_domingo_010 = [{id:0,nombre:"TODOS"},{id:1,nombre:"NARRADOR"},{id:3,n
 
 var primer_domingo_escenas = 
 [
-	{id: "001", orden:1, nombre:"EL PRIMER MILAGRO, EN LA BODA DE CANÁ", dialogos:dialogos_escena_el_primer_milagro, personajes:primer_domingo_001, video:""},
+	{id: "001", orden:1, nombre:"EL PRIMER MILAGRO, EN LA BODA DE CANÁ", dialogos:dialogos_escena_el_primer_milagro, personajes:primer_domingo_001, video:"https://youtu.be/zupv23KwdRg?si=F_oAFstE3QmPkzFh"},
 	{id: "002", orden:2, nombre:"JESÚS SE DESPIDE DE SU MADRE EN NAZARET", dialogos:dialogos_escena_jesus_se_despide_de_su_madre, personajes:primer_domingo_002, video:""},
 	{id: "003", orden:3, nombre:"PRESENTACIÓN DE JUAN A ISRAEL", dialogos:dialogos_escena_presentacion_de_juan_a_israel, personajes:primer_domingo_003, video:"https://youtube.com/watch?v=fHr4-KSEJLo&feature=shared"},
 	{id: "004", orden:4, nombre:"BAUTISMO DE JESÚS", dialogos:dialogos_escena_bautismo_de_jesus, personajes:primer_domingo_004, video:""},
-	{id: "005", orden:5, nombre:"LA TENTACIÓN EN EL DESIERTO", dialogos:dialogos_escena_tentacion_en_el_decierto, personajes:primer_domingo_005, video:""},
+	{id: "005", orden:5, nombre:"LA TENTACIÓN EN EL DESIERTO", dialogos:dialogos_escena_tentacion_en_el_decierto, personajes:primer_domingo_005, video:"https://youtu.be/jt2RPUASiac?si=p6M6R2OjNCFO4lgx"},
 	{id: "006", orden:6, nombre:"LOS PRIMEROS DISCIPULOS", dialogos:dialogos_escena_primeros_dicipulos, personajes:primer_domingo_006, video:""},
 	{id: "007", orden:7, nombre:"LA PESCA MILAGROSA", dialogos:dialogos_escena_la_pesca_milagrosa, personajes:primer_domingo_007, video:""},
 	{id: "008", orden:8, nombre:"LA HEMORROISA Y LA HIJA DE JAIRO (Mateo 9:18,25)", dialogos:dialogos_escena_la_emorroisa_y_la_hija_de_jairo, personajes:primer_domingo_008, video:""},
