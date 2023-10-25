7d371f78add49e929adeff0de0f10cc5a1f79bc3
jjluiisc <75454478+jjluiisc@users.noreply.github.com>
2023-10-25 10:19:16 -0600

    Update sistema.js
    No dejar copiar

--- js/sistema.js
+++ js/sistema.js
@@ -190,3 +190,14 @@ function Pdf(id,fileName){
 	doc.fromHTML(document.getElementById(id).innerHTML.replaceAll("<salto>","</br>"),5,5);
 	doc.save(fileName+".pdf");
 }
+
+document.addEventListener("contextmenu", function(event){
+	event.preventDefault();
+}, false);
+
+document.addEventListener("copy", function(event){
+	// Change the copied text if you want
+	event.clipboardData.setData("text/plain", "No se permite copiar en esta página web");
+	// Prevent the default copy action
+	event.preventDefault();
+}, false);
