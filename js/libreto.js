c04b90ea6dcd0f3d66b8264993c9db2e66a752c5
jjluiisc <75454478+jjluiisc@users.noreply.github.com>
2023-10-25 23:00:57 -0600

    Update libreto.js

--- js/libreto.js
+++ js/libreto.js
@@ -18,10 +18,10 @@ var primer_domingo_escenas =
 	{id: "004", orden:4, nombre:"BAUTISMO DE JESÚS", dialogos:dialogos_escena_bautismo_de_jesus, personajes:primer_domingo_004, video:""},
 	{id: "005", orden:5, nombre:"LA TENTACIÓN EN EL DESIERTO", dialogos:dialogos_escena_tentacion_en_el_decierto, personajes:primer_domingo_005, video:"https://youtu.be/jt2RPUASiac?si=p6M6R2OjNCFO4lgx"},
 	{id: "006", orden:6, nombre:"LOS PRIMEROS DISCIPULOS", dialogos:dialogos_escena_primeros_dicipulos, personajes:primer_domingo_006, video:"https://youtu.be/8U-TqdL74bc?si=-oUsIVXzmWs4LRDa"},
-	{id: "007", orden:7, nombre:"LA PESCA MILAGROSA", dialogos:dialogos_escena_la_pesca_milagrosa, personajes:primer_domingo_007, video:""},
-	{id: "008", orden:8, nombre:"LA HEMORROISA Y LA HIJA DE JAIRO (Mateo 9:18,25)", dialogos:dialogos_escena_la_emorroisa_y_la_hija_de_jairo, personajes:primer_domingo_008, video:""},
-	{id: "009", orden:9, nombre:"JESÚS Y LA SAMARITANA", dialogos:dialogos_escena_jesus_y_la_samaritana, personajes:primer_domingo_009, video:""},
-	{id: "010", orden:10, nombre:"LA ELECCIÓN DE LOS APÓSTOLES", dialogos:dialogos_escena_eleccion_de_los_apostoles, personajes:primer_domingo_010, video:""}
+	{id: "007", orden:7, nombre:"LA PESCA MILAGROSA", dialogos:dialogos_escena_la_pesca_milagrosa, personajes:primer_domingo_007, video:"https://youtu.be/J5ORKiYw_Is?si=5OxwbU9n4M7IdkQL"},
+	{id: "008", orden:8, nombre:"LA HEMORROISA Y LA HIJA DE JAIRO (Mateo 9:18,25)", dialogos:dialogos_escena_la_emorroisa_y_la_hija_de_jairo, personajes:primer_domingo_008, video:"https://youtu.be/Q_xjzrtXY3c?si=3umj0dgLr0dGq1kn"},
+	{id: "009", orden:9, nombre:"JESÚS Y LA SAMARITANA", dialogos:dialogos_escena_jesus_y_la_samaritana, personajes:primer_domingo_009, video:"https://youtu.be/98V5swouRtg?si=_N1kH69VGIgeuh32"},
+	{id: "010", orden:10, nombre:"LA ELECCIÓN DE LOS APÓSTOLES", dialogos:dialogos_escena_eleccion_de_los_apostoles, personajes:primer_domingo_010, video:"https://youtube.com/watch?v=x2YbIdzsHBg&feature=share"}
 ];
 
 	
@@ -47,7 +47,7 @@ var segundo_domingo_escenas =
 	{id: "016", orden:6, nombre:"MULTIPLICACIÓN DE LOS PANES Y PECES", dialogos:dialogos_escena_multiplicacion_de_panes_y_peces, personajes:segundo_domingo_006, video:""},
 	{id: "017", orden:7, nombre:"JESÚS Y EL CENTURIÓN ROMANO", dialogos:dialogos_escena_jesus_y_el_centurion_romano, personajes:segundo_domingo_007, video:""},
 	{id: "018", orden:8, nombre:"PECADORA ARREPENTIDA", dialogos:dialogos_escena_la_pecadora_arrepentida, personajes:segundo_domingo_008, video:""},
-	{id: "019", orden:9, nombre:"VUELTA A BETANIA", dialogos:dialogos_escena_vuelta_a_betania, personajes:segundo_domingo_009, video:""},
+	{id: "019", orden:9, nombre:"VUELTA A BETANIA", dialogos:dialogos_escena_vuelta_a_betania, personajes:segundo_domingo_009, video:"https://youtu.be/-MOBIL4mYiA?si=Ir4kl234WkFSFrey"},
 	{id: "020", orden:10, nombre:"SALOME", dialogos:dialogos_escena_salome, personajes:segundo_domingo_010, video:""}
 ];
 
