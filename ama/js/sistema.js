664a2c70d873370c475741476a7c85aa14fda622
jjluiisc <75454478+jjluiisc@users.noreply.github.com>
2024-05-09 14:05:49 -0600

    Update sistema.js

--- ama/js/sistema.js
+++ ama/js/sistema.js
@@ -2,7 +2,6 @@ $('#mainContent').empty();
 $(".site-header").show();
 
 
-var menu = [hoja_1];
 var max = 40;
 
 function cargaMenu(){
@@ -13,8 +12,12 @@ function cargaMenu(){
 		
 		salida += '<div class="row" style="background: url(img/'+hoja.img+'.jpg); background-repeat:no-repeat; background-size:100% 100%; background-attachment: fixed; min-height="100%">'
 			productos = hoja.productos;
-			salida += '<div class="col-sm-12 col-md-12 col-lg-12" align="center"><font style="color: #D43854; font-family: Pussycat, Algerian, Broadway; font-size: 4rem;">MEN&Uacute;</font></div>' +
-				      '<div class="col-sm-12 col-md-12 col-lg-12" align="center"><font style="color: #D43854; font-family: Pussycat, Algerian, Broadway; font-size: 3rem;">'+hoja.nombre+'</font></div>' ;
+			if(hoja.orientacion == 'L'){
+				salida += '<div class="col-sm-12 col-md-12 col-lg-12" align="center"><font style="color: #D43854; font-family: Pussycat, Algerian, Broadway; font-size: 4rem;">MEN&Uacute;</font></div>';
+				salida += '<div class="col-sm-12 col-md-12 col-lg-12" align="center"><font style="color: #D43854; font-family: Pussycat, Algerian, Broadway; font-size: 3rem;">'+hoja.nombre+'</font></div>' ;
+			}else if(hoja.orientacion == 'C'){
+				salida += '<div class="col-sm-12 col-md-12 col-lg-12" align="center"><font style="color: #D43854; font-family: Pussycat, Algerian, Broadway; font-size: 3rem;">'+hoja.nombre+'</font></div>' ;
+			}
 			for(var y=0; y < productos.length; y++){
 				var producto = productos[y];
 				var largo = producto.nombre.length;
@@ -24,20 +27,30 @@ function cargaMenu(){
 					renglones += 1;
 				}
 				var nombre = producto.nombre;				
-				var puntos = completaCadena(nombre);				
-				
-				salida += '<div class="col-sm-1 col-md-1 col-lg-1" style="padding-left:4em">&nbsp;</div>'+
+				var puntos = completaCadena(nombre);	
+
+				if(hoja.orientacion == 'L'){
+					salida += '<div class="col-sm-1 col-md-1 col-lg-1" style="padding-left:2em;">&nbsp;</div>'+
 						  '<div class="col-sm-7 col-md-7 col-lg-7">'+
 								'<font style="color: #D43854; font-family: '+"'"+'Algeria'+"'"+', sans-serif; font-size: 1.9rem;">'+nombre+'</font>'+
-								'<font style="color: #FE1D17; font-family: '+"'"+'Algeria'+"'"+', sans-serif; font-size: 1.8rem;">&nbsp;'+puntos+'</font>'+								
-								'<font style="color: #D43854; font-family: '+"'"+'Algeria'+"'"+', sans-serif; font-size: 1.9rem;">&nbsp;$'+producto.precio+'</font>'+
+								'<font style="color: #FE1D17; font-family: Pussycat, Algerian, Broadway; font-size: 1.8rem;">&nbsp;'+puntos+'</font>'+								
+								'<font style="color: #D43854; font-family: Pussycat, Algerian, Broadway; font-size: 1.9rem;">&nbsp;$'+producto.precio+'</font>'+
 						  '</div>' +
-				          '<div class="col-sm-4 col-md-4 col-lg-4">&nbsp;</div>'+
-						  '<div class="col-sm-1 col-md-1 col-lg-1">&nbsp;</div>'+
+						  '<div class="col-sm-4 col-md-4 col-lg-4">&nbsp;</div>'+
+						  // Segundo renglon
+						  '<div class="col-sm-1 col-md-1 col-lg-1" style="padding-left:2em;">&nbsp;</div>'+
 						  '<div class="col-sm-4 col-md-4 col-lg-4">'+
 								'<font style="color: #DE697A; font-family: Areal; font-size: 1.4rem;">'+producto.descripcion+'</font>'+
 						  '</div>' +
 				          '<div class="col-sm-7 col-md-7 col-lg-7">&nbsp;</div>';
+				}else if(hoja.orientacion == 'C'){
+					salida += '<div class="col-sm-4 col-md-4 col-lg-4" style="padding-left:2em;">&nbsp;</div>'+
+						  '<div class="col-sm-4 col-md-4 col-lg-4">'+
+								'<font style="color: #D43854; font-family: '+"'"+'Algeria'+"'"+', sans-serif; font-size: 1.9rem;">'+nombre+'</font>'+
+						  '</div>' +
+						  '<div class="col-sm-4 col-md-4 col-lg-4">&nbsp;</div>';
+				}
+				
 			}
 	    salida += '</div>';
 		
