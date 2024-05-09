a615223b50fc209376724843bdb4c94a86ce1cec
jjluiisc <75454478+jjluiisc@users.noreply.github.com>
2024-05-09 14:05:08 -0600

    Update menu.js

--- ama/js/menu.js
+++ ama/js/menu.js
@@ -1,16 +1,59 @@
 var productos_1 = [
-					{nombre:"AHUACATL", precio:37, descripcion:"Guacamole con totopos y cebolla."},
-					{nombre:"CHORIQUESO", precio:75, descripcion:"Queso oaxaca con chorizo y tortillas de harina."},
-					{nombre:"QUESO FUNDIDO", precio:60, descripcion:"Queso oaxaca y tortillas de harina."},
-					{nombre:"NACHOS SENCILLOS", precio:55, descripcion:"150 grs de totopos con queso amarillo, cebolla, jitomate y chiles curados."},
-					{nombre:"NACHOS C/ARRACHERA", precio:115, descripcion:"150 grs de arrachera c/150 grs de totopos, queso amarillo, cebolla, jitomate y chiles curados."},
-					{nombre:"NACHOS C/CHORIZO", precio:75, descripcion:"150 grs de chorizo c/ 150 gras de totopos, queso amarillo, cebolla, jitomate y chiles curados."},
-					{nombre:"NACHOS CAMPECHANOS", precio:115, descripcion:"100 grs de arrachera, 100 grs de chorizo, 150 grs de totopos, queso amarillo, cebolla, jitomate y chiles curados."},
-					{nombre:"PAPAS A LA FRANCESA", precio:55, descripcion:"250 grs de papas, queso amarillo y salsas."},
-					{nombre:"PAPAS GAJO", precio:65, descripcion:"250 grs de papas, queso amarillo y salsas."},
-					{nombre:"PAPAS A LA FRANCESA C/QUESO Y TOCINO", precio:85, descripcion:"250 grs de papas, queso manchego derretido, tocino, queso parmesano, queso amarillo, aderezo y salsas."},
-					{nombre:"PAPAS GAJO C/QUESO Y TOCINO", precio:95, descripcion:"250 grs de papas gajo, queso manchego derretido, tocino, queso parmesano, queso amarillo, aderezo y salsas."},
-					{nombre:"DEDOS DE QUESO", precio:67, descripcion:"6 pzs de dedos de queso manchego empanizados servidos con aderezo."}
+					{id:1, nombre:"AHUACATL", precio:37, descripcion:"Guacamole con totopos y cebolla."},
+					{id:2, nombre:"CHORIQUESO", precio:75, descripcion:"Queso oaxaca con chorizo y tortillas de harina."},
+					{id:3, nombre:"QUESO FUNDIDO", precio:60, descripcion:"Queso oaxaca y tortillas de harina."},
+					{id:4, nombre:"NACHOS SENCILLOS", precio:55, descripcion:"150 grs de totopos con queso amarillo, cebolla, jitomate y chiles curados."},
+					{id:5, nombre:"NACHOS C/ARRACHERA", precio:115, descripcion:"150 grs de arrachera c/150 grs de totopos, queso amarillo, cebolla, jitomate y chiles curados."},
+					{id:6, nombre:"NACHOS C/CHORIZO", precio:75, descripcion:"150 grs de chorizo c/ 150 gras de totopos, queso amarillo, cebolla, jitomate y chiles curados."},
+					{id:7, nombre:"NACHOS CAMPECHANOS", precio:115, descripcion:"100 grs de arrachera, 100 grs de chorizo, 150 grs de totopos, queso amarillo, cebolla, jitomate y chiles curados."},
+					{id:8, nombre:"PAPAS A LA FRANCESA", precio:55, descripcion:"250 grs de papas, queso amarillo y salsas."},
+					{id:9, nombre:"PAPAS GAJO", precio:65, descripcion:"250 grs de papas, queso amarillo y salsas."},
+					{id:10, nombre:"PAPAS A LA FRANCESA C/QUESO Y TOCINO", precio:85, descripcion:"250 grs de papas, queso manchego derretido, tocino, queso parmesano, queso amarillo, aderezo y salsas."},
+					{id:11, nombre:"PAPAS GAJO C/QUESO Y TOCINO", precio:95, descripcion:"250 grs de papas gajo, queso manchego derretido, tocino, queso parmesano, queso amarillo, aderezo y salsas."},
+					{id:12, nombre:"DEDOS DE QUESO", precio:67, descripcion:"6 pzs de dedos de queso manchego empanizados servidos con aderezo."}
                   ];
+				  
+var productos_2 = [
+					{id:13, nombre:"PEPITO DE PECHUGA", precio:117, descripcion:"Pan gourmet con 150 grs. con pechuga asada, frijoles, queso gouda, cebolla caramelizada, guacamole, lechuga, jitomate. Se sirve con 100 grs. de papas gajo."},
+					{id:14, nombre:"PEPITO DE CHORIZO", precio:119, descripcion:"Pan gourmet con 150 grs. con chorizo, frijoles, queso gouda, cebolla caramelizada, guacamole, lechuga, jitomate. Se sirve con 100 grs. de papas gajo."},
+					{id:15, nombre:"PEPITO DE ARRACHERA", precio:139, descripcion:"Pan gourmet con 150 grs. con arrachera, frijoles, queso gouda, cebolla caramelizada, guacamole, lechuga, jitomate. Se sirve con 100 grs. de papas gajo."},
+					{id:16, nombre:"CLUB S&Aacute;NDWICH PECHUGA", precio:123, descripcion:"Tres rebanadas de pan de la casa relleno de pechuga de pollo asada, jam&oacute;n de pavo, tocino, queso gouda, mayonesa, lechuga, jitomate, cebolla.  Se sirve con 150 grs. de papas a la francesa."},
+					{id:17, nombre:"CLUB S&Aacute;NDWICH CARNES FR&Iacute;AS", precio:133, descripcion:"Tres rebanadas de pan de la casa relleno de salami, salchicha, jam&oacute;n de pavo, tocino, queso gouda, mayonesa, lechuga, jitomate, cebolla.  Se sirve con 150 grs. de papas a la francesa."},
+					{id:18, nombre:"CLUB S&Aacute;NDWICH EMPANIZADO", precio:127, descripcion:"Tres rebanadas de pan de la casa relleno de pechuga de pollo empanizada, jam&oacute;n de pavo, tocino, queso gouda, mayonesa, lechuga, jitomate, cebolla.  Se sirve con 150 grs. de papas a la francesa."},
+					{id:19, nombre:"BA&Ntilde;ALO CON QUESO", precio:15, descripcion:"Ba&ntilde;a de queso gouda el pepito o s&aacute;ndwich de tu preferencia."},
+					{id:20, nombre:"PONLE HUEVITO", precio:10, descripcion:"Acompa&ntilde;a de huevo al gusto el s&aacute;ndwich favorito."},
+					{id:21, nombre:"BONELESS", precio:97, descripcion:"200 grs de cuadros de pechuga preparados como alitas ba&ntilde;ados en salsa de tu elecci&oacute;n."},
+					{id:22, nombre:"	1 KG BONELESS", precio:395, descripcion:"1 kg de cuadros de pechuga pechuga preparados como alitas ba&ntilde;ados en salsa de tu elecci&oacute;n."},
+					{id:23, nombre:"ALITAS", precio:87, descripcion:"6 pzs alitas ba&ntilde;ados en salsa de tu elecci&oacute;n."},
+					{id:24, nombre:"	24 PZS ALITAS", precio:295, descripcion:"Alitas ba&ntilde;ados en salsa de tu elecci&oacute;n."},
+					
+					{id:39, nombre:"ADEREZO EXTRA", precio:10, descripcion:""},
+					{id:40, nombre:"SALSA EXTRA", precio:10, descripcion:""},
+					{id:41, nombre:"PAPAS A LA FRANCESA", precio:35, descripcion:"100 grs de papas, queso amarillo y salsas."},
+					{id:42, nombre:"PAPAS GAJO", precio:39, descripcion:"100 grs de papas gajo, queso amarillo y salsas."},
+                  ];				  
+				  
+var productos_3 = [
+					{id:25, nombre:"(*) MANGO HABANERO", precio:0, descripcion:""},
+					{id:26, nombre:"(*) BBQ HABANERO", precio:0, descripcion:""},
+					{id:27, nombre:"(*) HABANERO CREAM", precio:0, descripcion:""},
+					{id:28, nombre:"(*) X HOT", precio:0, descripcion:""},
+					{id:29, nombre:"(*) BUFALO", precio:0, descripcion:""},
+					{id:30, nombre:"(*) MANGO HOT", precio:0, descripcion:""},
+					{id:31, nombre:"(*) TAMARINDO CHIPOTLE", precio:0, descripcion:""},
+					{id:32, nombre:"(*) MEZCAL", precio:0, descripcion:""},
+					{id:33, nombre:"(*) BBQ", precio:0, descripcion:""},
+					{id:34, nombre:"(*) AJO PARMESANO", precio:0, descripcion:""},
+					{id:35, nombre:"(*) ALITAS", precio:0, descripcion:""},
+					{id:36, nombre:"(*) PIMIENTA & LIM&Oacute;N", precio:0, descripcion:""},
+					
+					{id:37, nombre:" * ADEREZO RANCH", precio:0, descripcion:""},
+					{id:38, nombre:" * ADEREZO QUESO AZUL", precio:0, descripcion:""},
+                  ];				  				  
 
-var hoja_1 = {img:"fondo", nombre:"PA' BOTANIAR", productos:productos_1};
+var hoja_1 = {img:"fondo", orientacion :"L", nombre:"PA' BOTANIAR", productos:productos_1};
+var hoja_2 = {img:"fondo", orientacion :"L", nombre:"PEPITOS Y CLUB S&Aacute;NDWICH", productos:productos_2};
+var hoja_3 = {img:"fondo", orientacion :"C", nombre:"SALSAS", productos:productos_3};
+
+
+var menu = [hoja_1,hoja_2,hoja_3];
