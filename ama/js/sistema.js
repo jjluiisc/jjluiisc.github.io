ab75b348da9263433fdec01179b83b6ff69816bd
jjluiisc <75454478+jjluiisc@users.noreply.github.com>
2024-05-10 18:21:25 -0600

    Update sistema.js

--- ama/js/sistema.js
+++ ama/js/sistema.js
@@ -7,17 +7,25 @@ var max = 40;
 function cargaMenu(){
 	
 	var salida = '';
+	
+	
 	for(var x=0; x < menu.length; x++){
 		var hoja = menu[x];
 		
 		salida += '<div class="row" style="background: url(img/'+hoja.img+'.jpg); background-repeat:no-repeat; background-size:100% 100%; background-attachment: fixed; min-height="100%">'
 			productos = hoja.productos;
 			if(hoja.orientacion == 'L'){
+				
+				//salida += '';
 				salida += '<div class="col-sm-12 col-md-12 col-lg-12" align="center"><font style="color: #D43854; font-family: Pussycat, Algerian, Broadway; font-size: 4rem;">MEN&Uacute;</font></div>';
 				salida += '<div class="col-sm-12 col-md-12 col-lg-12" align="center"><font style="color: #D43854; font-family: Pussycat, Algerian, Broadway; font-size: 3rem;">'+hoja.nombre+'</font></div>' ;
 			}else if(hoja.orientacion == 'C'){
 				salida += '<div class="col-sm-12 col-md-12 col-lg-12" align="center"><font style="color: #D43854; font-family: Pussycat, Algerian, Broadway; font-size: 3rem;">'+hoja.nombre+'</font></div>' ;
 			}
+			
+			salida +=   '	<div class="col-sm-11 col-md-11 col-lg-8" style="padding-left:2em;">'+
+						'		<table style="width:100%;" >';
+			
 			for(var y=0; y < productos.length; y++){
 				var producto = productos[y];
 				var largo = producto.nombre.length;
@@ -28,21 +36,22 @@ function cargaMenu(){
 				}
 				var nombre = producto.nombre;				
 				var puntos = completaCadena(nombre);	
+				
 
 				if(hoja.orientacion == 'L'){
-					salida += '<div class="col-sm-1 col-md-1 col-lg-1" style="padding-left:2em;">&nbsp;</div>'+
-						  '<div class="col-sm-7 col-md-7 col-lg-7">'+
-								'<font style="color: #D43854; font-family: '+"'"+'Algeria'+"'"+', sans-serif; font-size: 1.9rem;">'+nombre+'</font>'+
-								'<font style="color: #FE1D17; font-family: Pussycat, Algerian, Broadway; font-size: 1.8rem;">&nbsp;'+puntos+'</font>'+								
-								'<font style="color: #D43854; font-family: Pussycat, Algerian, Broadway; font-size: 1.9rem;">&nbsp;$'+producto.precio+'</font>'+
-						  '</div>' +
-						  '<div class="col-sm-4 col-md-4 col-lg-4">&nbsp;</div>'+
-						  // Segundo renglon
-						  '<div class="col-sm-1 col-md-1 col-lg-1" style="padding-left:2em;">&nbsp;</div>'+
-						  '<div class="col-sm-4 col-md-4 col-lg-4">'+
-								'<font style="color: #DE697A; font-family: Areal; font-size: 1.4rem;">'+producto.descripcion+'</font>'+
-						  '</div>' +
-				          '<div class="col-sm-7 col-md-7 col-lg-7">&nbsp;</div>';
+					salida +=	'		<tr style="width:100%;">'+
+								'			<td style="max-width:20%;">'+
+								'				<div style ="width:300px; white-space:nowrap; overflow:hidden;">'+
+								'					<font style="color: #D43854; font-family: '+"'"+'Algeria'+"'"+', sans-serif; font-size: '+calculaTamanio(nombre)+'rem;">'+nombre+'</font>'+
+								'					<font style="color: #FE1D17; font-family: Pussycat, Algerian, Broadway; font-size: 1.7rem;">&nbsp;'+puntos+'</font>'+																
+								'				</div>'+
+								'			</td>'+
+								'			<td style="max-width:10%;">'+
+								'					<font style="color: #D43854; font-family: Pussycat, Algerian, Broadway; font-size: 1.8rem;">&nbsp;$'+producto.precio+'</font>'+
+								'			</td>'+
+								'			<td style="max-width:70%;"></td>'+
+								'		</tr>';
+								
 				}else if(hoja.orientacion == 'C'){
 					salida += '<div class="col-sm-4 col-md-4 col-lg-4" style="padding-left:2em;">&nbsp;</div>'+
 						  '<div class="col-sm-4 col-md-4 col-lg-4">'+
@@ -52,10 +61,14 @@ function cargaMenu(){
 				}
 				
 			}
+			
+		salida += '		</table>'+
+				  '	</div>'+
+				  '	<div class="col-sm-1 col-md-1 col-lg-4"></div>';
+			
 	    salida += '</div>';
 		
 	}
-	
 	
 	$('#mainContent').append(salida);
 	
@@ -64,16 +77,24 @@ function cargaMenu(){
 function completaCadena(nombre){
 	var cadena = "";	
 	var largo = nombre.length;
-	var faltantes = max - largo;
+	var faltantes = max-largo;
 	for(var x=0; x < faltantes; x++){
-		cadena+="." 
+		cadena+=" ." 
 	}
 	return cadena; 
 }
 
+function calculaTamanio(nombre){
+	var tamanio = 1.9;	
+	var largo = nombre.length;
+	var faltantes = largo-10;
+	if(faltantes > 0)
+		tamanio = tamanio - (faltantes * 0.02 )
+	return tamanio; 
+}
+
 
 cargaMenu();
-
 document.addEventListener("contextmenu", function(event){
 	event.preventDefault();
 }, false);
