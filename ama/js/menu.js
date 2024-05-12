ebe89c24e3a55c2fc6a929be3cac31c2d0d9a522
jjluiisc <75454478+jjluiisc@users.noreply.github.com>
2024-05-11 20:22:14 -0600

    Update menu.js

--- ama/js/menu.js
+++ ama/js/menu.js
@@ -50,10 +50,63 @@ var productos_3 = [
 					{id:37, nombre:" * ADEREZO RANCH", precio:0, descripcion:""},
 					{id:38, nombre:" * ADEREZO QUESO AZUL", precio:0, descripcion:""},
                   ];				  				  
+				  
+
+var productos_4 = [
+					{id:43, nombre:"CANTARITO CL&Aacute;SICO", precio:85, descripcion:"Mezcal de Tequila, jugo de toronja, jugo de naranja, jugo de lim&oacute;n, un toque de chamoy y agua mineral. Escarchado con tamarindo y acompa&ntilde;ado con una banderilla de tamarindo. 480 ml."},
+					{id:44, nombre:"CANTARITO SABOR", precio:85, descripcion:"Mezcal de Tequila, jugo de toronja, jugo de lim&oacute;n, jarabe natural y agua mineral. MANGO C/FRESA, JAMAICA C/ARANDANO. 480ml."},
+					{id:45, nombre:"MUCHO AMOR BERRY SANGR&Iacute;A", precio:85, descripcion:"Vino tinto, licor de frambuesa, jugo de naranja y ar&aacute;ndano. 480ml."},
+					{id:46, nombre:"MOJITO CL&Aacute;SICO 1/2L", precio:75, descripcion:"Mezcla de Ronblanco, hierbabuena, lim&oacute;n, agua mineral y jarabe natural. 500ml."},
+					{id:47, nombre:"MOJITO CL&Aacute;SICO 1L", precio:125, descripcion:"Mezcla de Ronblanco, hierbabuena, lim&oacute;n, agua mineral y jarabe natural. 1000ml."},
+					{id:48, nombre:"MOJITO PONLE SABOR 1/2L", precio:85, descripcion:"Mezcla de Ronblanco, hierbabuena, lim&oacute;n, agua mineral y jarabe natural. (fresa, frutos rojos, mango) 500ml."},
+					{id:49, nombre:"MOJITO PONLE SABOR 1L", precio:135, descripcion:"Mezcla de Ronblanco, hierbabuena, lim&oacute;n, agua mineral y jarabe natural. (fresa, frutos rojos, mango) 1000ml."},
+					{id:50, nombre:"MARGACARIBE", precio:95, descripcion:"Granizado de fruta natural, jarabe de frutas, se sirve con caribe y adornado con fruta natural. 480 ml."},
+					{id:51, nombre:"MARGACHELA", precio:85, descripcion:"Granizado de fruta natural, jarabe de frutas, se sirve con caribe y adornado con fruta natural. 480 ml."},
+					{id:52, nombre:"PI&Ntilde;A COLADA", precio:95, descripcion:"Ron blanco, crema de coco, agua de coco, pi&ntilde;a natural, jugo natural y endulzante. 480 ml."},
+					{id:53, nombre:"CERVEZA BOLA 1/2L", precio:59, descripcion:"Cerveza de tu elecci&oacute;n en copa escarchada con tamarindo o chamoy y ajonjol&iacute;."},
+					{id:54, nombre:"CERVEZA 1L", precio:95, descripcion:"Cerveza de tu elecci&oacute;n en copa escarchada con tamarindo o chamoy y ajonjol&iacute;."},
+					{id:55, nombre:"VASO CUBANO 1/2L", precio:15, descripcion:""},
+					{id:56, nombre:"VASO CUBANO 1L", precio:25, descripcion:""},
+					{id:57, nombre:"CARIBE", precio:49, descripcion:""},
+                  ];
+				  
+var productos_5 = [
+					{id:58, nombre:"SODA ITALIANA", precio:65, descripcion:"Agua mineral con escencia de tu preferencia (FRESA, FRUTOS ROJOS, MANGO, BRUE BERRY) fruta natural y hielo. 470 ml."},
+					{id:59, nombre:"LIMONADA CL&Aacute;SICA", precio:47, descripcion:"Jugo de lim&oacute; con jarabe natural y agua mineral. Acomp&aacute;&ntilde;ala con fruta de temporada. 480 ml."},
+					{id:60, nombre:"LIMONADA FRUTA DE TEMPORADA", precio:57, descripcion:""},
+					{id:61, nombre:"NARANJADA CL&Aacute;SICA", precio:47, descripcion:"Jugo de naranja con jarabe natural y agua mineral. Acomp&aacute;&ntilde;ala con fruta de temporada. 480 ml."},
+					{id:62, nombre:"NARANJADA FRUTA DE TEMPORADA", precio:57, descripcion:""},
+					{id:63, nombre:"MALTEADA", precio:57, descripcion:"Se sirve con crema batida, chocolate, cereza y bast&oacute;n de chocolate (FRESA, CHOCOLATE, VAINILLA, COOKIES & CREAM)."},
+					{id:64, nombre:"T&Eacute; HELADO FRUTOS ROJOS", precio:35, descripcion:"T&eacute; de frutos rojos granizado o a las rocas, dulce."},
+					{id:65, nombre:"FRAPES", precio:77, descripcion:"Se sirve en vaso escarchado con chocolate, crema batida, cereza y bast&oacute;n de chocolate (FRAPUCHINO, CHICHAROS DULCES, OREOFRUTOS ROJOS, MAZAPAN)."},
+					{id:66, nombre:"REFRESCO", precio:25, descripcion:""},
+					{id:67, nombre:"REFRESCO PREPARADO", precio:35, descripcion:"Refreso en vaso escarchado con lim&oacute;n y sal o chamoy y ajonjol&iacute;"},
+                  ];
+				  
+var productos_6 = [
+					{id:68, nombre:"T&Eacute;", precio:65, descripcion:"T&eacute; de diversos sabores y aromas (Manzana, Jamaica, Lim&oacute;n, jengibe, manzanilla, menta)"},
+					{id:69, nombre:"CAF&Eacute;", precio:57, descripcion:"Caf&eacute; de Chiapas."},
+					{id:70, nombre:"CAPUCHINO", precio:47, descripcion:""},
+					{id:71, nombre:"CAF&Eacute; DALGONA CL&Aacute;SICO", precio:57, descripcion:"Caf&eacute; dulce espumoso con leche."},
+					{id:72, nombre:"CAF&Eacute; DALGONA", precio:67, descripcion:"Caf&eacute; dulce espumoso con leche (FRUTOS ROJOS, NUTELLA, OREO)."},
+					{id:73, nombre:"AFOGATO", precio:43, descripcion:"Helado de vainilla con caf&eacute;."},
+                  ];				  
+
+
+var productos_7 = [
+					{id:74, nombre:"PASTELES;", precio:65, descripcion:""},
+					{id:75, nombre:"BROWNIE SUNDAE", precio:65, descripcion:"Brownie con helado de vainilla y adornado con tiras de chocolate, nuez y fresa."},
+					{id:76, nombre:"BROWNIE BLANCO", precio:65, descripcion:"Brownie de chocolate blanco servido con helado de vainilla y adornado con tiras de chocolate almendra y fresa."}
+                  ];				  
+				  
 
 var hoja_1 = {img:"fondo", orientacion :"L", nombre:"PA' BOTANIAR", productos:productos_1};
 var hoja_2 = {img:"fondo", orientacion :"L", nombre:"PEPITOS Y CLUB S&Aacute;NDWICH", productos:productos_2};
 var hoja_3 = {img:"fondo", orientacion :"C", nombre:"SALSAS", productos:productos_3};
+var hoja_4 = {img:"fondo", orientacion :"L", nombre:"BEBIDAS FR&Iacute;AS CON ALCOHOL", productos:productos_4};
+var hoja_5 = {img:"fondo", orientacion :"L", nombre:"BEBIDAS FR&Iacute;AS SIN ALCOHOL", productos:productos_5};
+var hoja_6 = {img:"fondo", orientacion :"L", nombre:"BEBIDAS CALIENETES Y FR&Iacute;AS", productos:productos_6};
+var hoja_7 = {img:"fondo", orientacion :"L", nombre:"POSTRES", productos:productos_7};
 
 
-var menu = [hoja_1,hoja_2,hoja_3];
+var menu = [hoja_1,hoja_2,hoja_3,hoja_4,hoja_5,hoja_6,hoja_7];
